df54219b879327969cd4252227c667c01483b1b5
aya <a.tamura.sys22@morijyobi.ac.jp>
2022-11-12 21:58:54 +0900

    Main.javaの内容追記

--- Main.java
+++ Main.java
@@ -5,10 +5,10 @@ public class  Main{
 
 		for (int i = 1; i <= 2; i++){
 			System.out.println(i+"人目");
-				System.out.print("身長(cm)=");
+				System.out.print("身長(cm)= ");
 				double height = sc.nextDouble() / 100;
 				
-				System.out.print("体重(kg)=");
+				System.out.print("体重(kg)= ");
 				double weight = sc.nextDouble();
 
 				double bmi = weight / height / height;
