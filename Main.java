b4071a0f2f5bc69f0e25daecb1aa8c291e3dcda0
aya <a.tamura.sys22@morijyobi.ac.jp>
2022-11-12 21:38:56 +0900

    Main.java̕ҏW

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class  Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		for (int i = 1; i <= 2; i++){
+			System.out.println(i+"l–Ú");
+				System.out.print("g’·(cm)=");
+				double height = sc.nextDouble() / 100;
+				
+				System.out.print("‘Ìd(kg)=");
+				double weight = sc.nextDouble();
+
+				double bmi = weight / height / height;
+				double b = ((double)Math.round(bmi * 100))/100;
+				
+				System.out.println(b);
+		}
+	}
+}
